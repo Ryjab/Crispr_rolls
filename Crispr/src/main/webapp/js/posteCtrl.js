01dbebe39e33050b962313bca050c4b65286d123
Labassi Elyès <eLabassi@MacBook-de-Labassi.local>
2017-02-23 16:40:16 +0100

    adre

--- Crispr/src/main/webapp/js/posteCtrl.js
+++ Crispr/src/main/webapp/js/posteCtrl.js
@@ -1,15 +1,26 @@
-function posteCtrl ($scope) {
-    //requete a faire
-    $scope.poste =
-        {
-            //remplir ces champs avec les valeurs du formulaire
-            titre: '',
-            repoImage: 'repoImage essai',
-            contenu: ''
-        };
-    $scope.soumettreFormulairePoste = function (poste){
-        $scope.titre = poste.titre;
-        console.log(poste);
-        console.log($scope.titre);
-    }
+function posteCtrl ($scope, $http) {
+        $scope.poste =
+            {
+                //remplir ces champs avec les valeurs du formulaire
+                id_article: 0,
+                titre: '',
+                repoImage: 'repoImage essai',
+                contenu: ''
+            };
+        $scope.soumettreFormulairePoste = function (poste) {
+            $http.post('http://localhost:8080/CreateArticle').then(function (data) {
+                console.log(data);
+                $scope.id_article = poste.id_article;
+                $scope.titre = poste.titre;
+                $scope.repoImage = poste.repoImage;
+                $scope.contenu = poste.contenu;
+                console.log(poste);
+                console.log($scope.id_article);
+                console.log($scope.repoImage);
+                console.log($scope.titre);
+                console.log($scope.contenu);
+            }).error(function (reject) {
+                console.log(reject);
+            });
+        }
 }
